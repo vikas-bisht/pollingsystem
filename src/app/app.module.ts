a6982f6b6cb4e3ee5445346cc1961a3339d628f9
Vikas <vikas_b@excellencetechnologies.in>
2017-09-26 18:30:39 +0530

    initial step for login form and used bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HashLocationStrategy, LocationStrategy} from '@angular/common';
 
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule, AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [{provide: LocationStrategy,useClass:HashLocationStrategy}],
+  bootstrap: [AppComponent,RegisterComponent]
 })
 export class AppModule { }
